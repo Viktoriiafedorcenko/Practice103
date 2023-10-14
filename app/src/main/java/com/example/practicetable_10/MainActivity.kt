6452da0c63cfa6a0968980ce851ca2a8eede1a50
Your Name <you@example.com>
2023-10-14 10:15:52 +0300

    first commit

---
+++ app/src/main/java/com/example/practicetable_10/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.practicetable_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tablelayout)
+        setTitle("TableLayout")
+    }
+}
